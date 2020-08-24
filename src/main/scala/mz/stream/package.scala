31bb39b46a0a75747575af22d583cfd65eda6591
Zhang <ztz2000@gmail.com>
2020-08-24 22:20:50 +0800

    [new] draft kafka stream consumer

--- src/main/scala/mz/stream/package.scala
+++ src/main/scala/mz/stream/package.scala
@@ -5,4 +5,7 @@ package object stream {
   val sparkAppName = "Structured Streaming from Kafka"
   val kafkaTopic = "trades"
   val kafkaBootstrapServers: String = "localhost:9092"
+  val kafkaStreamInterval = "30 seconds"
+  val kafkaStreamOutputMode = "update"
+  val parquetPath = "/"
 }
